b00cd79ce5c71b86cc621f4d2d94b551cace3773
Ryan Berger <ryan.berger@techdata.com>
2019-06-12 13:00:09 -0400

    Adding PS Scripts

---
+++ archive/get-sa-connection-string.ps1
@@ -0,0 +1,9 @@
+﻿#Set Resource Group Name and Storage Account Name
+$rgName = "RB-archive"
+$saName = "saksh6j3vmllzlw"
+#Login to Az account
+Login-AzAccount
+#Build Storage Connection String - your string will be the last line of the script output. Starting with 'Default' and ending with 'windows.net'. 
+$sa = Get-AzStorageAccount -StorageAccountName $saName -ResourceGroupName $rgName
+$saKey = (Get-AzStorageAccountKey -ResourceGroupName $rgName -Name $saName)[0].Value
+'DefaultEndpointsProtocol=https;AccountName=' + $saName + ';AccountKey=' + $saKey + ';EndpointSuffix=core.windows.net' 
